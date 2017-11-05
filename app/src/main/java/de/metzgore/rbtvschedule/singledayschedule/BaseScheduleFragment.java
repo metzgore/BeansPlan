1e1c3fa5e3df0638ca27b25ddaafaa889548e254
Nico Riemer <nicoriemer+github@gmail.com>
2017-11-05 19:05:32 +0100

    Fix updating base schedules in weekly schedule fragment

--- app/src/main/java/de/metzgore/rbtvschedule/singledayschedule/BaseScheduleFragment.java
+++ app/src/main/java/de/metzgore/rbtvschedule/singledayschedule/BaseScheduleFragment.java
@@ -10,29 +10,33 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import de.metzgore.rbtvschedule.R;
 import de.metzgore.rbtvschedule.dailyschedule.ScheduleAdapter;
 import de.metzgore.rbtvschedule.data.Show;
+import de.metzgore.rbtvschedule.data.WeeklySchedule;
 import de.metzgore.rbtvschedule.databinding.LayoutScheduleBaseBinding;
+import de.metzgore.rbtvschedule.shared.UpdatableScheduleFragment;
 
-public class BaseScheduleFragment extends Fragment {
+public class BaseScheduleFragment extends Fragment implements UpdatableScheduleFragment {
 
     private static final String TAG = BaseScheduleFragment.class.getSimpleName();
 
     private static final String ARG_SCHEDULE = "arg_schedule";
+    private static final String ARG_DATE = "arg_date";
 
-    private LayoutScheduleBaseBinding binding;
-
+    private Date dateKey;
     private List<Show> showList;
     private ScheduleAdapter scheduleAdapter;
 
-    public static Fragment newInstance(List<Show> shows) {
+    public static Fragment newInstance(Date date, List<Show> shows) {
         BaseScheduleFragment fragment = new BaseScheduleFragment();
 
         Bundle args = new Bundle();
         args.putParcelableArrayList(ARG_SCHEDULE, (ArrayList) shows);
+        args.putLong(ARG_DATE, date.getTime());
 
         fragment.setArguments(args);
 
@@ -49,6 +53,10 @@ public class BaseScheduleFragment extends Fragment {
 
         if (args != null) {
             showList = args.getParcelableArrayList(ARG_SCHEDULE);
+            dateKey = new Date(args.getLong(ARG_DATE));
+        } else {
+            showList = new ArrayList<>();
+            dateKey = new Date();
         }
     }
 
@@ -56,7 +64,7 @@ public class BaseScheduleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        binding = DataBindingUtil.inflate(inflater, R.layout.layout_schedule_base, container, false);
+        LayoutScheduleBaseBinding binding = DataBindingUtil.inflate(inflater, R.layout.layout_schedule_base, container, false);
 
         binding.showsList.addItemDecoration(new DividerItemDecoration(getActivity(),
                 DividerItemDecoration.VERTICAL));
@@ -71,5 +79,14 @@ public class BaseScheduleFragment extends Fragment {
     public void onStart() {
         super.onStart();
         scheduleAdapter.setShowList(showList);
+    }
+
+    @Override
+    public void update(WeeklySchedule weeklySchedule) {
+        for (Date key : weeklySchedule.getSchedule().keySet()) {
+            if (key.equals(dateKey)) {
+                scheduleAdapter.setShowList(weeklySchedule.getSchedule().get(key));
+            }
+        }
     }
 }
--- app/src/main/java/de/metzgore/rbtvschedule/weeklyschedule/WeeklyScheduleAdapter.java
+++ app/src/main/java/de/metzgore/rbtvschedule/weeklyschedule/WeeklyScheduleAdapter.java
@@ -9,6 +9,7 @@ import android.text.format.DateUtils;
 import java.util.Date;
 
 import de.metzgore.rbtvschedule.data.WeeklySchedule;
+import de.metzgore.rbtvschedule.shared.UpdatableScheduleFragment;
 import de.metzgore.rbtvschedule.singledayschedule.BaseScheduleFragment;
 
 import static android.text.format.DateUtils.FORMAT_SHOW_DATE;
@@ -33,13 +34,21 @@ class WeeklyScheduleAdapter extends FragmentStatePagerAdapter {
     @Override
     public Fragment getItem(int position) {
         Date selectedDate = (Date) weeklySchedule.getSchedule().keySet().toArray()[position];
-        return BaseScheduleFragment.newInstance(weeklySchedule.getSchedule().get(selectedDate));
+        return BaseScheduleFragment.newInstance(selectedDate, weeklySchedule.getSchedule().get(selectedDate));
     }
 
     @Override
     public String getPageTitle(int position) {
         Date dateOfSchedule = (Date) weeklySchedule.getSchedule().keySet().toArray()[position];
         return DateUtils.formatDateTime(context, dateOfSchedule.getTime(), FORMAT_SHOW_WEEKDAY | FORMAT_SHOW_DATE);
+    }
+
+    @Override
+    public int getItemPosition(Object object) {
+        if (object instanceof UpdatableScheduleFragment) {
+            ((UpdatableScheduleFragment) object).update(weeklySchedule);
+        }
+        return super.getItemPosition(object);
     }
 
     public void setSchedule(WeeklySchedule weeklySchedule) {
