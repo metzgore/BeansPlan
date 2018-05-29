a2198f40d07fa55276a70d0067025ada74174f8f
Nico Riemer <nicoriemer+github@gmail.com>
2018-05-29 20:11:12 +0200

    Execute callbacks in onActivityCreated tp ensure activity is there

--- app/src/main/java/de/metzgore/beansplan/baseschedule/RefreshableScheduleFragment.java
+++ app/src/main/java/de/metzgore/beansplan/baseschedule/RefreshableScheduleFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.view.View;
 
 public abstract class RefreshableScheduleFragment extends Fragment {
 
@@ -21,22 +20,16 @@ public abstract class RefreshableScheduleFragment extends Fragment {
             throw new ClassCastException(context.toString()
                     + " must implement OnScheduleRefreshedListener");
         }
-
-        callback.onExpandAppBar();
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        callback.onSubTitleUpdated(null);
-        callback.onLastUpdateUpdated(0);
-        callback.onRemovePaddingBottom();
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         callback.onAddToolbarElevation();
+        callback.onExpandAppBar();
+        callback.onSubTitleUpdated(null);
+        callback.onLastUpdateUpdated(0);
+        callback.onRemovePaddingBottom();
     }
 
     @NonNull
