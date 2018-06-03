f982a2dd7ef8248a9378659122e7c051e13b917e
Nico Riemer <nicoriemer+github@gmail.com>
2018-06-03 14:05:48 +0200

    Fix wrong view holder alpha
    Due to default item animator of the RecyclerView, the change animation set the alpha always to 1, overriding our changes

--- app/src/main/java/de/metzgore/beansplan/dailyschedule/DailyScheduleFragment.java
+++ app/src/main/java/de/metzgore/beansplan/dailyschedule/DailyScheduleFragment.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.SimpleItemAnimator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -81,6 +82,9 @@ public class DailyScheduleFragment extends Fragment implements UpdatableSchedule
         binding.fragmentBaseScheduleShowsList.addItemDecoration(new DividerItemDecoration(getActivity(),
                 DividerItemDecoration.VERTICAL));
         binding.fragmentBaseScheduleShowsList.setHasFixedSize(true);
+        ((SimpleItemAnimator) binding.fragmentBaseScheduleShowsList.getItemAnimator()).setSupportsChangeAnimations
+                (false);
+
         binding.fragmentBaseScheduleShowsList.setAdapter(dailyScheduleAdapter);
 
         return binding.getRoot();
