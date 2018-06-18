f13e956c2dbc66d2ed560fced53d5720df65e1cc
Nico Riemer <nicoriemer+github@gmail.com>
2018-06-18 13:30:12 +0200

    Fix date parsing on during deserialization on Android 5

--- app/src/main/java/de/metzgore/beansplan/weeklyschedule/WeeklyScheduleDaoImpl.kt
+++ app/src/main/java/de/metzgore/beansplan/weeklyschedule/WeeklyScheduleDaoImpl.kt
@@ -12,11 +12,11 @@ import de.metzgore.beansplan.util.GsonSerializer
 class WeeklyScheduleDaoImpl(context: Context, cacheOnly: Boolean = false) : WeeklyScheduleDao {
 
     private var weeklyScheduleCache: DualCache<WeeklySchedule> = if (cacheOnly) {
-        Builder<WeeklySchedule>(WeeklyScheduleDao.WEEKLY_SCHEDULE_KEY, 1).enableLog()
+        Builder<WeeklySchedule>(WeeklyScheduleDao.WEEKLY_SCHEDULE_KEY, 2).enableLog()
                 .useSerializerInRam(1000000, GsonSerializer(WeeklySchedule::class.java))
                 .noDisk().build()
     } else {
-        Builder<WeeklySchedule>(WeeklyScheduleDao.WEEKLY_SCHEDULE_KEY, 1).enableLog()
+        Builder<WeeklySchedule>(WeeklyScheduleDao.WEEKLY_SCHEDULE_KEY, 2).enableLog()
                 .useSerializerInRam(1000000, GsonSerializer(WeeklySchedule::class.java))
                 .useSerializerInDisk(1000000, true, GsonSerializer(WeeklySchedule::class.java),
                         context).build()
